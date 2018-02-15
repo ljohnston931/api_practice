4fa1c415301774c9c7063bab136ff34fb1fd07b3
Lucy Johnston <ljohnston931@gmail.com>
2018-02-15 14:03:10 -0700

    moved files inside other folder, search works and displays pictures

--- recipes/script.js
+++ recipes/script.js
@@ -12,6 +12,15 @@ $(document).ready(function() {
             dataType: "json",
             success: function(json) {
                 console.log(json);
+
+                var results = "";
+                results += "<h2>Recipes with " + value + ":</h2>";
+                for (var i=0; i < json.hits.length; i++)
+                {
+                    results += "<img src='" + json.hits[i].recipe.image + "'>"
+                    results += "<p><a href='" + json.hits[i].recipe.url + "'>" + json.hits[i].recipe.label + "</a></p>";
+                }
+                $("#recipeResult").html(results);
             }, //success function
         });  //ajax
     }); //click recipeSubmit
